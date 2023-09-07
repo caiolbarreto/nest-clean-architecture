bf3ed407d48ee9a518876d3f6d3e3db84a6d64e0
Caio Barreto <caiobarreto_28@hotmail.com>
2023-09-07 15:00:26 -0300

    use the env file to make requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true
-    })
+    }),
+    AuthModule
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthController],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+@Controller('/sessions')
+export class AuthController {
+  constructor(private jwt: JwtService) {}
+
+  @Post()
+  async handle() {
+    const token = this.jwt.sign({ sub: 'user-id' })
+
+    return token
+  }
+}
