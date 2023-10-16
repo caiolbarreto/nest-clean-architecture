d4f64e2d806a5f81634a42bfd6b53fc50b3c4c81
Caio Barreto <caiobarreto_28@hotmail.com>
2023-10-15 22:37:04 -0300

    creating the quetsion with details in domain layer

---
+++ src/domain/forum/enterprise/entities/value-objects/question-details.ts
@@ -0,0 +1,65 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ValueObject } from '@/core/entities/value-object'
+import { Slug } from './slug'
+import { Attachment } from '../attachment'
+
+export interface QuestionDetailsProps {
+  questionId: UniqueEntityID
+  author: {
+    id: UniqueEntityID
+    name: string
+  }
+  title: string
+  content: string
+  slug: Slug
+  attachments: Attachment[]
+  bestAnswerId?: UniqueEntityID | null
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class QuestionDetails extends ValueObject<QuestionDetailsProps> {
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get authorId() {
+    return this.props.author.id
+  }
+
+  get authorName() {
+    return this.props.author.name
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get attachments() {
+    return this.props.attachments
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(props: QuestionDetailsProps) {
+    return new QuestionDetails(props)
+  }
+}
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,14 +1,19 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
-import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { InMemoryAttachmentsRepository } from './in-memory-attachments-repository'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { InMemoryQuestionAttachmentsRepository } from './in-memory-question-attachments-repository'
+import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
   constructor(
-    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+    private questionAttachmentsRepository: InMemoryQuestionAttachmentsRepository,
+    private attachmentsRepository: InMemoryAttachmentsRepository,
+    private studentsRepository: InMemoryStudentsRepository,
   ) {}
 
   async findById(id: string) {
@@ -29,6 +34,55 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async findDetailsBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    const author = this.studentsRepository.items.find((student) =>
+      student.id.equals(question.authorId),
+    )
+
+    if (!author) {
+      throw new Error(`Author with name "${question.authorId}" does not exist.`)
+    }
+
+    const questionAttachments = this.questionAttachmentsRepository.items.filter(
+      (questionAttachment) => questionAttachment.questionId.equals(question.id),
+    )
+
+    const attachments = questionAttachments.map((questionAttachment) => {
+      const attachment = this.attachmentsRepository.items.find((attachment) =>
+        attachment.id.equals(questionAttachment.attachmentId),
+      )
+
+      if (!attachment) {
+        throw new Error(
+          `Attachment with name "${question.authorId}" does not exist.`,
+        )
+      }
+
+      return attachment
+    })
+
+    return QuestionDetails.create({
+      questionId: question.id,
+      author: {
+        id: author.id,
+        name: author.name,
+      },
+      title: question.title,
+      content: question.content,
+      slug: question.slug,
+      attachments,
+      bestAnswerId: question.bestAnswerId,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    })
   }
 
   async findManyRecent({ page }: PaginationParams) {
